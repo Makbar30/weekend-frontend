e0297996f3a1c449c17867a7183e22b8cc5c285c
Akbarbar30 <mohammadakbar28@yahoo.co.id>
2020-10-26 14:26:59 +0700

    Updates

---
+++ precache-manifest.0c4697211624cdc1f591fdaa7dd9f30b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fd3b1f474ea52292c499c7b783b523",
+    "url": "/weekend-frontend/index.html"
+  },
+  {
+    "revision": "b3e2cfe855b930ccdae8",
+    "url": "/weekend-frontend/static/css/2.e8cb318b.chunk.css"
+  },
+  {
+    "revision": "b24e92295107e0da73d7",
+    "url": "/weekend-frontend/static/css/main.4210bc0d.chunk.css"
+  },
+  {
+    "revision": "b3e2cfe855b930ccdae8",
+    "url": "/weekend-frontend/static/js/2.494617a1.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/weekend-frontend/static/js/2.494617a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b24e92295107e0da73d7",
+    "url": "/weekend-frontend/static/js/main.8d3165ab.chunk.js"
+  },
+  {
+    "revision": "a76075f69736cc70d993",
+    "url": "/weekend-frontend/static/js/runtime-main.7f255250.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/weekend-frontend/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/weekend-frontend/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/weekend-frontend/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/weekend-frontend/static/media/slick.f97e3bbf.svg"
+  }
+]);
